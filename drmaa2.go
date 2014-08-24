710bee13376219a5fcb2efc8a373a306294d3e71
Daniel Gruber <dgruber@univa.com>
2014-08-24 21:53:31 +0200

    DG: Fixed header.

--- drmaa2.go
+++ drmaa2.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2014 Daniel Gruber
+Copyright 2014 Daniel Gruber, http://www.gridengine.eu
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
